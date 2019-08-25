9ae59c7e4b27042845cadc7330054b09bc34d53b
Artur <5k2017.izi1999@gmail.com>
2019-08-25 18:49:05 +0300

    add all content

---
+++ js/main.js
@@ -0,0 +1,34 @@
+// accordion
+
+let acc = document.getElementsByClassName("accordion");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+    acc[i].addEventListener("click", function () {
+        this.classList.toggle("actives");
+        let panel = this.nextElementSibling;
+        if (panel.style.maxHeight) {
+            panel.style.maxHeight = null;
+        } else {
+            panel.style.maxHeight = panel.scrollHeight + "px";
+        }
+    });
+}
+
+// button up
+
+$(function () {
+    $(window).scroll(function () {
+        if ($(this).scrollTop() != 0) {
+            $('#toTop').fadeIn();
+        } else {
+            $('#toTop').fadeOut();
+        }
+    });
+
+    $('#toTop').click(function () {
+        $('body,html').animate({
+            scrollTop: 0
+        }, 1500);
+    });
+});
