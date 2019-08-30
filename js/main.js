f2aa0b7bacbc3437d3ec69cc395e4ccdac475914
Artur <5k2017.izi1999@gmail.com>
2019-08-30 08:13:24 +0300

    add work of the mistake 2.0

--- js/main.js
+++ js/main.js
@@ -32,3 +32,113 @@ $(function () {
         }, 1500);
     });
 });
+
+    // advantages counter
+
+$(document).ready(function(){   
+    for (var i = 0; i < $('.advantages__content').length; i++){
+        var block = $($('.advantages__content')[i]).find('.advantages__card');
+
+        if (i < 13) {
+            block.text(i+1 + "."); 
+        } else {
+            // URL для картинки может быть любым =)
+            block.html('<img src="picture.jpg" />');
+        }
+    }
+});
+
+    // map
+    function initMap() {
+      let map = new google.maps.Map(document.getElementById('map'), {
+        center: {lat: 40.674, lng: -73.945},
+        zoom: 12,
+        styles: [
+          {elementType: 'geometry', stylers: [{color: '#242f3e'}]},
+          {elementType: 'labels.text.stroke', stylers: [{color: '#242f3e'}]},
+          {elementType: 'labels.text.fill', stylers: [{color: '#746855'}]},
+          {
+            featureType: 'administrative.locality',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#d59563'}]
+          },
+          {
+            featureType: 'poi',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#d59563'}]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'geometry',
+            stylers: [{color: '#263c3f'}]
+          },
+          {
+            featureType: 'poi.park',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#6b9a76'}]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry',
+            stylers: [{color: '#38414e'}]
+          },
+          {
+            featureType: 'road',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#212a37'}]
+          },
+          {
+            featureType: 'road',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#9ca5b3'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry',
+            stylers: [{color: '#746855'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'geometry.stroke',
+            stylers: [{color: '#1f2835'}]
+          },
+          {
+            featureType: 'road.highway',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#f3d19c'}]
+          },
+          {
+            featureType: 'transit',
+            elementType: 'geometry',
+            stylers: [{color: '#2f3948'}]
+          },
+          {
+            featureType: 'transit.station',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#d59563'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'geometry',
+            stylers: [{color: '#17263c'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.fill',
+            stylers: [{color: '#515c6d'}]
+          },
+          {
+            featureType: 'water',
+            elementType: 'labels.text.stroke',
+            stylers: [{color: '#17263c'}]
+          }
+        ]
+      });
+    }
+
+    // function initMap() {
+    //   let uluru = {lat: 40.674, lng: -73.945};
+    //   let map = new google.maps.Map(
+    //       document.getElementById('map'), {zoom: 12, center: uluru});
+    //   let marker = new google.maps.Marker({position: uluru, map: map});
+    // }
